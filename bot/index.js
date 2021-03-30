e290df72f5fdb15c6138fd0a1fac4eb1e5217d86
ducktrshessami <jwee761@gmail.com>
2021-03-30 16:40:25 -0700

    boilerplate bot code

---
+++ bot/index.js
@@ -0,0 +1,20 @@
+const DiscordBot = require("discord-bot");
+const config = require("../config/bot.json");
+const commands = require("./commands");
+
+var client;
+
+client = new DiscordBot(config, commands);
+
+client.on("ready", function () {
+    console.log(`Logged in as ${client.user.username}#${client.user.discriminator}`);
+});
+
+client.on("shardDisconnect", function () {
+    console.log("Logged off");
+    process.exit();
+});
+
+client.on("error", console.error);
+
+module.exports = client;
