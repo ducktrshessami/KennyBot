c3059777ffed82c8fb0112c35ad77bead7d2fb2e
ducktrshessami <jwee761@gmail.com>
2021-04-01 22:30:32 -0700

    setup cycler

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const Cycle = require("express-cycle");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const cycler = Cycle({
+    origin: process.env.API_ORIGIN || `http://localhost:${PORT}`,
+    ms: 1500000,
+    verbose: true
+});
+
+app.use(cycler);
 
 module.exports = app.listen(PORT, function () {
     console.log(`API listening on PORT ${PORT}`);
+    cycler.startLoop();
 });
