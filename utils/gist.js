c89a0a55917415fe988ed8ec14ae9b4d71692a21
ducktrshessami <jwee761@gmail.com>
2021-04-01 20:53:39 -0700

    removed setup for gist based playlist handling

--- utils/gist.js
+++
@@ -1,8 +0,0 @@
-const { Octokit } = require("@octokit/rest");
-
-let client = new Octokit();
-
-// Set personal access token
-module.exports.setToken = function(token) {
-    client = new Octokit({ auth: token });
-}
