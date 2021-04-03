37c2b0fc99c761c5b0dabf6c69378a5310a9f5e3
ducktrshessami <jwee761@gmail.com>
2021-04-03 02:27:19 -0700

    handle token refresh

--- api/utils/discord.js
+++ api/utils/discord.js
@@ -1,3 +1,4 @@
+const phin = require("phin");
 const hash = require("./hash");
 
 const scope = "identify guilds";
@@ -31,8 +32,11 @@ function preAuth(req, res, next) {
 
 function authCheck(req, res, next) {
     if (req.session.discord.access_token) {
-        if (req.session.discord.expiry - Date.now()) {
+        if ((new Date(req.session.discord.expiry)) - Date.now()) {
             refresh(req, res, next);
+        }
+        else {
+            next();
         }
     }
     else {
@@ -41,7 +45,26 @@ function authCheck(req, res, next) {
 }
 
 function refresh(req, res, next) {
-    next();
+    phin({
+        url: "https://discord.com/api/oauth2/token",
+        method: "post",
+        data: (new URLSearchParams({
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            grant_type: "refresh_token",
+            refresh_token: req.session.discord.refresh_token,
+            redirect_uri: process.env.CLIENT_REDIRECT,
+            scope: discord.scope
+        })).toString(),
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        parse: "json"
+    })
+        .then(tokenRes => {
+            req.session.discord.access_token = tokenRes.body.access_token;
+            req.session.discord.expiry = new Date(Date.now() + (tokenRes.body.expires_in * 1000));
+            req.session.discord.refresh_token = tokenRes.body.refresh_token;
+            next();
+        });
 }
 
 module.exports = {
