3f05c95d620999616bf5b9b1cf5b2073f12ee748
ducktrshessami <jwee761@gmail.com>
2021-04-02 23:26:25 -0700

    handle login with oauth redirect

--- api/routes/discord.js
+++ api/routes/discord.js
@@ -1,3 +1,12 @@
-module.exports = function (router) {
+const phin = require("phin");
+const discord = require("../utils/discord");
 
+module.exports = function (router) {
+    router.get("/api/login", discord.preLogin, function (req, res) {
+        res.redirect(discord.authUrl);
+    });
+
+    router.get("/api/auth", function (req, res) {
+
+    });
 };
