80845f71a27d4c3b45219512c3f85918558ea4fd
ducktrshessami <jwee761@gmail.com>
2021-04-02 17:12:25 -0700

    dummy react Home page

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import {
+  HashRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
+
+import Home from "./pages/Home";
 import './App.css';
 
+function handleSearchCode(search) {
+  let params = new URLSearchParams(search);
+  let code = params.get("code");
+  if (code) {
+    // handle auth
+    params.delete("code");
+    window.location.search = params.toString();
+  }
+}
+
 function App() {
+  handleSearchCode(window.location.search);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
