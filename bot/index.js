5919aeb5ed635ffdcc6cbc8b5e2c7849f997b575
ducktrshessami <jwee761@gmail.com>
2021-04-04 03:14:32 -0700

    guild info stored to db from bot cache

--- bot/index.js
+++ bot/index.js
@@ -1,6 +1,7 @@
 const DiscordBot = require("discord-bot");
 const config = require("../config/bot.json");
 const commands = require("./commands");
+const initGuild = require("./utils/initGuild");
 
 var client;
 
@@ -19,6 +20,7 @@ client = new DiscordBot(config, commands);
 
 client.on("ready", function () {
     console.log(`Logged in as ${client.user.username}#${client.user.discriminator}`);
+    client.guilds.cache.each(initGuild);
 });
 
 client.on("shardDisconnect", function () {
@@ -27,5 +29,9 @@ client.on("shardDisconnect", function () {
 });
 
 client.on("error", console.error);
+
+client.on("guildCreate", initGuild);
+
+client.on("guildUpdate", (before, after) => initGuild(after));
 
 module.exports = client;
