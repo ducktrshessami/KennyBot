2f271fc20e97200780d8ba80d68a02af18f125a2
ducktrshessami <jwee761@gmail.com>
2021-04-03 18:33:44 -0700

    init user in db on user data get

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,11 +1,15 @@
 const discord = require("../utils/discord");
+const user = require("../utils/user");
 
 module.exports = function (router) {
     router.get("/api/user", discord.authCheck, function (req, res) {
         discord.getUser(req.session.discord.access_token)
             .then(userResponse => {
                 if (userResponse.statusCode === 200) {
-                    res.status(200).json(userResponse.body);
+                    return user.initUser(userResponse.body)
+                        .then(() => {
+                            res.status(200).json(userResponse.body);
+                        });
                 }
                 else {
                     res.status(401).end();
