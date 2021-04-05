2ea5bff714edcd07a209d477e4d59e71619b6df0
ducktrshessami <jwee761@gmail.com>
2021-04-05 10:32:20 -0700

    login redirect handling

--- react-client/src/components/LoginButton/index.js
+++ react-client/src/components/LoginButton/index.js
@@ -1,8 +1,10 @@
+import { useHistory } from "react-router-dom";
 // import Logo from "./Discord-Logo-White.svg";
 import "./LoginButton.css";
 
 export default function LoginButton() {
+    const history = useHistory();
     return (
-        <button className="btn btn-login">Login With Discord</button>
+        <button className="btn btn-login" onClick={() => history.push("/login")}>Login With Discord</button>
     );
 };
