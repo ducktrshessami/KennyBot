38f156126a86c4a7d90938421134acbb0147f1e2
ducktrshessami <jwee761@gmail.com>
2021-04-05 16:31:28 -0700

    main prune command flow

--- bot/commands/prune.js
+++ bot/commands/prune.js
@@ -1,7 +1,35 @@
-const { Command } = require("discord-bot");
+const { Command, utils } = require("discord-bot");
+
+function getFilteredMessages(channel, filterIDs) {
+
+}
 
 module.exports = new Command("prune", function (message, args) {
-    
+    let count = Number(args[1]);
+    if (count && count <= 100) {
+        let targets = count;
+        let filterIDs = args.slice(2)
+            .join(" ")
+            .match(/<@[0-9]+>|<@![0-9]+>/g)
+            .map(mention => mention.match(/[0-9]+/)[0]);
+        if (filterIDs.length) {
+            targets = getFilteredMessages(message.channel, filterIDs);
+        }
+        message.channel.bulkDelete(targets, true)
+            .then(deleted => {
+                return utils.sendVerbose(message.channel, `Pruned \`${deleted.size}\` messages. This message will self-destruct in 5 seconds`)
+                    .then(response => response.delete({ timeout: 5000 }));
+            })
+            .catch(err => {
+                console.error(err);
+                utils.sendVerbose(message.channel, "There was an error pruning messages")
+                    .catch(console.error);
+            });
+    }
+    else {
+        utils.sendVerbose(message.channel, `${this.usage}\n${this.subtitle}`)
+            .catch(console.log);
+    }
 }, {
     usage: "@kennybot prune <number> [user]",
     description: "Deletes up to a given number of messages. The deleted messages can be filtered to a specified user",
