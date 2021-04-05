2f8f69a6c2eb9937fac37920ba97d8ed7b9f23fa
ducktrshessami <jwee761@gmail.com>
2021-04-04 17:48:40 -0700

    roll command

---
+++ bot/commands/roll.js
@@ -0,0 +1,22 @@
+const { Command, utils } = require("discord-bot");
+const { maxDice, roll, generateReply } = require("../utils/diceHandler");
+
+module.exports = new Command("roll", function (message, args) {
+    let query = "d20", result;
+    if (args.length > 1) {
+        args.shift();
+        query = args.join("");
+    }
+    if (result = roll(query)) {
+        utils.sendVerbose(message.channel, generateReply(message.author.id, result))
+            .catch(console.log);
+    }
+    else {
+        utils.sendVerbose(message.channel, `${this.usage}\n${this.subtitle}`)
+            .catch(console.log);
+    }
+}, {
+    usage: "@kennybot roll [die count]d<sides>[modifiers]",
+    description: "Rolls dice. Defaults to a d20 if nothing is specified",
+    subtitle: `Max dice count is ${maxDice}`
+});
