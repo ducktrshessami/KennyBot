4d63311bb61a77ffe03f07d2899a16519ddce260
ducktrshessami <jwee761@gmail.com>
2021-04-05 17:56:10 -0700

    advantage command

---
+++ bot/commands/advantage.js
@@ -0,0 +1,23 @@
+const { Command, utils } = require("discord-bot");
+const { maxDice, advantage, generateReply } = require("../utils/diceHandler");
+
+module.exports = new Command("advantage", function (message, args) {
+    let query = "d20", results;
+    if (args.length > 1) {
+        args.shift();
+        query = args.join("");
+    }
+    if (results = advantage(query)) {
+        utils.sendVerbose(message.channel, generateReply(message.author.id, results[0], results[1].value))
+            .catch(console.log);
+    }
+    else {
+        utils.sendVerbose(message.channel, `\`${this.usage}\`\n${this.subtitle}`)
+            .catch(console.log);
+    }
+}, {
+    usage: "@kennybot advantage [die count]d<sides>[modifiers]",
+    description: "Rolls dice with advantage. Defaults to a d20 if nothing is specified",
+    subtitle: `Max dice count is ${maxDice}`,
+    aliases: ["adv"]
+});
