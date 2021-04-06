7204912f3003b903dcc037c4a4ee78b04d87e8f4
ducktrshessami <jwee761@gmail.com>
2021-04-05 19:04:50 -0700

    stat command

---
+++ bot/commands/stat.js
@@ -0,0 +1,18 @@
+const { Command, utils } = require("discord-bot");
+const { maxDice, stat } = require("../utils/diceHandler");
+
+module.exports = new Command("stat", function (message, args) {
+    let count = Number(args[1]) || 4, result;
+    if (result = stat(count)) {
+        utils.sendVerbose(message.channel, "<@" + message.author.id + "> `" + result + "`")
+            .catch(console.log);
+    }
+    else {
+        utils.sendVerbose(message.channel, `\`${this.usage}\`\n${this.subtitle}`)
+            .catch(console.log);
+    }
+}, {
+    usage: "@kennybot stat [dice count]",
+    description: "Rolls a single stat based on a number of d6s, best three summed with 1s rerolled once",
+    subtitle: `Defaults to 4d6. Max dice count is ${maxDice}`
+});
