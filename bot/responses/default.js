7e4175d62965c3d8c5899d485bc6e9275cae12ee
ducktrshessami <jwee761@gmail.com>
2021-04-09 14:31:47 -0700

    cleaned up unused options for default res

--- bot/responses/default.js
+++ bot/responses/default.js
@@ -3,8 +3,4 @@ const { Response, utils } = require("discord-bot");
 module.exports = new Response(["kenny"], undefined, undefined, function (message) {
     utils.sendVerbose(message.channel, this.client.emojis.cache.random().toString())
         .catch(console.error);
-}, {
-    serverWhitelist: [
-
-    ]
 });
