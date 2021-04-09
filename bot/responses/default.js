bf454eba384dce260c67190fd590ec2f2f762373
ducktrshessami <jwee761@gmail.com>
2021-04-09 13:47:57 -0700

    default response sends random emoji

---
+++ bot/responses/default.js
@@ -0,0 +1,10 @@
+const { Response, utils } = require("discord-bot");
+
+module.exports = new Response("kenny", undefined, undefined, function (message) {
+    utils.sendVerbose(message.channel, this.client.emojis.cache.random().toString())
+        .catch(console.error);
+}, {
+    serverWhitelist: [
+
+    ]
+});
