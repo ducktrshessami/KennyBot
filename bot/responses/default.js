6c163c633660a4917c1686d5f3bc8409ea750d50
ducktrshessami <jwee761@gmail.com>
2021-04-09 14:30:32 -0700

    Revert "default response server whitelist handled by env or config"
    This reverts commit e6dbffb4d639a55475c252a907c9f48868be60e5.
    
    not using whitelist for responses

--- bot/responses/default.js
+++ bot/responses/default.js
@@ -1,19 +1,10 @@
 const { Response, utils } = require("discord-bot");
-const config = require("../../config/bot.json");
-
-try {
-    let parsedServers = JSON.parse(process.env.BOT_DEFRESLIST);
-    if (parsedServers) {
-        config.resOptions.default.servers = parsedServers;
-    }
-}
-catch {
-    console.warn("Could not parse env BOT_DEFRESLIST as JSON");
-}
 
 module.exports = new Response(["kenny"], undefined, undefined, function (message) {
     utils.sendVerbose(message.channel, this.client.emojis.cache.random().toString())
         .catch(console.error);
 }, {
-    serverWhitelist: config.resOptions.default.servers
+    serverWhitelist: [
+
+    ]
 });
