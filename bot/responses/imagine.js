3d7bbb2fd485e946bcf99e433393a7bdd0625433
ducktrshessami <jwee761@gmail.com>
2021-04-09 14:51:24 -0700

    env for dragons whitelist

--- bot/responses/imagine.js
+++ bot/responses/imagine.js
@@ -1,9 +1,20 @@
 const { Response, utils } = require("discord-bot");
 const cooldown = require("with-cooldown").default;
+const config = require("../../config/bot.json");
+
+try {
+    let parsedDragons = JSON.parse(process.env.BOT_DRAGONS);
+    if (parsedDragons) {
+        config.resOptions.imagine.dragons = parsedDragons;
+    }
+}
+catch {
+    console.warn("Could not parse env BOT_DRAGONS as JSON");
+}
 
 module.exports = new Response(["imagine"], "Just imagine", undefined, cooldown(60000, function (message, response) {
     utils.sendVerbose(message.channel, response)
         .catch(console.error);
 }), {
-
+    serverBlacklist: config.resOptions.imagine.dragons
 });
