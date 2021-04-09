e6dbffb4d639a55475c252a907c9f48868be60e5
ducktrshessami <jwee761@gmail.com>
2021-04-09 13:51:13 -0700

    default response server whitelist handled by env or config

--- bot/responses/default.js
+++ bot/responses/default.js
@@ -1,10 +1,19 @@
 const { Response, utils } = require("discord-bot");
+const config = require("../../config/bot.json");
+
+try {
+    let parsedServers = JSON.parse(process.env.BOT_DEFRESLIST);
+    if (parsedServers) {
+        config.resOptions.default.servers = parsedServers;
+    }
+}
+catch {
+    console.warn("Could not parse env BOT_DEFRESLIST as JSON");
+}
 
 module.exports = new Response("kenny", undefined, undefined, function (message) {
     utils.sendVerbose(message.channel, this.client.emojis.cache.random().toString())
         .catch(console.error);
 }, {
-    serverWhitelist: [
-
-    ]
+    serverWhitelist: config.resOptions.default.servers
 });
