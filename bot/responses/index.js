a81701b6696ecb05f830d8c50ed53efd78a7b890
ducktrshessami <jwee761@gmail.com>
2021-04-09 13:33:15 -0700

    allow priority reigstration for bot responses

--- bot/responses/index.js
+++ bot/responses/index.js
@@ -1,12 +1,9 @@
-const fs = require("fs");
 const path = require("path");
 
+const priority = [
+    "default"
+];
+
 module.exports =
-    fs
-        .readdirSync(__dirname)
-        .filter(file => (
-            (file.indexOf('.') !== 0) &&
-            (file !== path.basename(__filename)) &&
-            (file.slice(-3) === '.js')
-        ))
-        .map(file => require(path.join(__dirname, file)));
+    priority
+        .map(file => require(path.join(__dirname, file + ".js")));
