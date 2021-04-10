22f8b8c184c7ee65d890236b344cdea952ae0deb
ducktrshessami <jwee761@gmail.com>
2021-04-09 21:32:38 -0700

    generated help command sorts now

--- bot/index.js
+++ bot/index.js
@@ -19,7 +19,6 @@ catch {
 
 // Special tailored help command
 // commands.push(helpCmd(commands));
-commands.sort();
 
 config.token = process.env.BOT_TOKEN || config.token;
 client = new DiscordBot(config, commands, responses);
