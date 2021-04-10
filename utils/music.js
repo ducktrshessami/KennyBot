0be380c974a95d3f399bea3434009b02832e9547
ducktrshessami <jwee761@gmail.com>
2021-04-10 00:08:03 -0700

    descriptive music util returns

--- utils/music.js
+++ utils/music.js
@@ -12,19 +12,29 @@ function changeVolume(guildID, volume) {
     let guild = findGuild(guildID);
     if (guild && guild.voice.connection && guild.voice.connection.dispatcher) {
         guild.voice.connection.dispatcher.setVolume(volume);
+        return true;
     }
+    return false;
 }
 
 function pause(guildID) {
     let guild = findGuild(guildID);
     if (guild && guild.voice.connection && guild.voice.connection.dispatcher) {
-        guild.voice.connection.dispatcher.pause(true);
+        if (!guild.voice.connection.dispatcher.paused) {
+            guild.voice.connection.dispatcher.pause(true);
+            return true;
+        }
     }
+    return false;
 }
 
 function resume(guildID) {
     let guild = findGuild(guildID);
     if (guild && guild.voice.connection && guild.voice.connection.dispatcher) {
-        guild.voice.connection.dispatcher.resume();
+        if (guild.voice.connection.dispatcher.paused) {
+            guild.voice.connection.dispatcher.resume();
+            return true;
+        }
     }
+    return false;
 }
