2b117222a18252f5d61a8fdae58c06cb5014cc21
ducktrshessami <jwee761@gmail.com>
2021-04-09 18:33:22 -0700

    animated botmin command

---
+++ bot/commands/animated.js
@@ -0,0 +1,23 @@
+const { Command, utils } = require("discord-bot");
+
+module.exports = new Command("animated", function (message, args) {
+    let results = [];
+    args.slice(1).forEach(query => {
+        let target = message.guild.emojis.cache.find(emoji => emoji.name.toLowerCase() === query.toLowerCase());
+        if (target) {
+            results.push(target);
+        }
+    });
+    if (results.length) {
+        utils.sendVerbose(message.channel, results.map(emoji => `${emoji}: ${emoji.id}`).join("\n"))
+            .catch(console.error);
+    }
+    else {
+        utils.sendVerbose(message.channel, `${message.author}\n\`${this.usage}\``)
+            .catch(console.error);
+    }
+}, {
+    admin: true,
+    usage: "@kennybot animated <emoji names>",
+    description: "Get the id of an animated emoji"
+});
