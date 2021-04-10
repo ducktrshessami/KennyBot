0a4a12e0588a7170a578cbc6327b45512b3d5eac
ducktrshessami <jwee761@gmail.com>
2021-04-09 21:06:29 -0700

    join command

---
+++ bot/commands/join.js
@@ -0,0 +1,15 @@
+const { Command, utils } = require("discord-bot");
+
+module.exports = new Command("join", function (message) {
+    if (message.member.voice.channel && !message.member.voice.channel.members.has(client.user.id)) {
+        message.member.voice.channel.join()
+            .then(connection => utils.sendVerbose(message.channel, `Connected to \`${connection.channel.name}\``))
+            .catch(console.error);
+    }
+    else {
+        utils.sendVerbose(message.channel, `${message.author} No`)
+            .catch(console.error);
+    }
+}, {
+
+});
