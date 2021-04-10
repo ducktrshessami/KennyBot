fb23ce6a7b5ba4738cc8732b22da354338cf23e5
ducktrshessami <jwee761@gmail.com>
2021-04-09 23:00:53 -0700

    prefix command with db

---
+++ bot/commands/prefix.js
@@ -0,0 +1,24 @@
+const { Command, utils } = require("discord-bot");
+const db = require("../../models");
+
+module.exports = new Command("prefix", function (message, args) {
+    db.Guild.findByPk(message.guild.id)
+        .then(guild => {
+            if (args.length > 1) {
+                this.client.config.servers[message.guild.id].prefix = args[1];
+                return guild.update({ prefix: args[1] })
+                    .then(() => utils.sendVerbose(message.channel, `Custom prefix set to \`${args[1]}\``));
+            }
+            else {
+                if (guild.prefix) {
+                    return utils.sendVerbose(message.channel, `Current custom prefix: \`${guild.prefix}\``);
+                }
+                else {
+                    return utils.sendVerbose(message.channel, "Custom prefix not set");
+                }
+            }
+        })
+        .catch(console.error);
+}, {
+
+});
