09e7cad098b75b3d239d09772b7cd8deba0bcdc5
ducktrshessami <jwee761@gmail.com>
2021-04-10 00:11:13 -0700

    invite is admin command now

--- bot/commands/invite.js
+++ bot/commands/invite.js
@@ -5,6 +5,7 @@ module.exports = new Command("invite", function (message) {
     utils.sendVerbose(message.channel, `${message.author} ${inviteUrl}`)
         .catch(console.error);
 }, {
+    admin: true,
     usage: "@kennybot invite",
     description: "Send a link to invite KennyBot to a server",
     aliases: ["inv"]
