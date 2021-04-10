40ade2514390d2fed668f88619f6386041b5cf90
ducktrshessami <jwee761@gmail.com>
2021-04-09 23:12:09 -0700

    read prefix to memory for command handling

--- bot/utils/initGuild.js
+++ bot/utils/initGuild.js
@@ -4,10 +4,11 @@ module.exports = function (guild) {
     return db.Guild.findByPk(guild.id)
         .then(dbGuild => {
             if (dbGuild) {
+                process.bot.config.servers[guild.id].prefix = dbGuild.prefix;
                 return dbGuild.update(guild);
             }
             else {
                 db.Guild.create(guild);
             }
-        })
+        });
 };
