58a4a94a7a5cecd9319510ae68ff9903b14d5709
ducktrshessami <jwee761@gmail.com>
2021-04-09 23:17:52 -0700

    prefix command meta

--- bot/commands/prefix.js
+++ bot/commands/prefix.js
@@ -4,7 +4,7 @@ const db = require("../../models");
 module.exports = new Command("prefix", function (message, args) {
     db.Guild.findByPk(message.guild.id)
         .then(guild => {
-            if (args.length > 1) {
+            if (args.length > 1 && message.author.id === message.guild.ownerID) {
                 this.client.config.servers[message.guild.id].prefix = args[1];
                 return guild.update({ prefix: args[1] })
                     .then(() => utils.sendVerbose(message.channel, `Custom prefix set to \`${args[1]}\``));
@@ -20,5 +20,7 @@ module.exports = new Command("prefix", function (message, args) {
         })
         .catch(console.error);
 }, {
-
+    usage: "@kennybot prefix [prefix]",
+    description: "View or change the command prefix",
+    subtitle: "Only the server owner can change the prefix"
 });
