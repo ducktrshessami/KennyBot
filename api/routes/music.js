5350c9051fa57e7277932edaebf1c75a6044519c
ducktrshessami <jwee761@gmail.com>
2021-04-09 19:54:10 -0700

    api route to change guild volume

--- api/routes/music.js
+++ api/routes/music.js
@@ -1,7 +1,25 @@
 const db = require("../../models");
 const auth = require("../middleware/auth");
+const music = require("../../utils/music");
 
 module.exports = function (router) {
+    router.post("/api/volume/:guildId/:volume", auth.authCheck, auth.authGuilds, function (req, res) {
+        if (req.authGuilds.find(guild => guild.id === req.params.guildId)) {
+            let vol = Math.max(0, Math.min(1.5, req.params.volume));
+            db.Guild.findByPk(req.params.guildId)
+                .then(guild => guild.update({ volume: vol }))
+                .then(() => music.changeVolume(req.params.guildId, vol))
+                .then(() => res.status(200).end())
+                .catch(err => {
+                    console.error(err);
+                    res.status(500).end();
+                });
+        }
+        else {
+            res.status(404).end();
+        }
+    });
+
     router.get("/api/playlists/:guildId", auth.authCheck, auth.authGuilds, function (req, res) {
         if (req.authGuilds.find(guild => guild.id === req.params.guildId)) {
             db.Guild.findByPk(req.params.guildId, {
