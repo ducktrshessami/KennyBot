31f9843c4a50e5c7a9653b6dafd5b822c8a456f5
ducktrshessami <jwee761@gmail.com>
2021-04-11 13:19:29 -0700

    Disclaimer styling

--- react-client/src/pages/Disclaimer/index.js
+++ react-client/src/pages/Disclaimer/index.js
@@ -1,9 +1,12 @@
+import "./Disclaimer.css";
+
 export default function Disclaimer() {
     return (
         <main>
-            <div className="disclaimer">
-                <p>I don't own any property associated with <a href="https://en.wikipedia.org/wiki/The_Walking_Dead_(video_game_series)">The Walking Dead</a></p>
-                <p>I originally named this bot "KennyBot" following an inside joke with some of my friends</p>
+            <div className="disclaimer center">
+                <h5>I don't own any property associated with <a href="https://en.wikipedia.org/wiki/The_Walking_Dead_(video_game_series)">The Walking Dead</a></h5>
+                <br />
+                <h5>I originally named this bot "KennyBot" following an inside joke with some of my friends</h5>
             </div>
         </main>
     );
