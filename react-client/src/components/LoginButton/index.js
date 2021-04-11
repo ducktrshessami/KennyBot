00a632e8de6df452fe9d7030883bd85c1c0e25f6
ducktrshessami <jwee761@gmail.com>
2021-04-11 11:52:05 -0700

    discord logo on login button

--- react-client/src/components/LoginButton/index.js
+++ react-client/src/components/LoginButton/index.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
-// import Logo from "./Discord-Logo-White.svg";
 import "./LoginButton.css";
 
 export default function LoginButton() {
     return (
-        <Link className="btn btn-login" to="/login">Login With Discord</Link >
+        <Link className="btn btn-login" to="/login">
+            Login With Discord
+            <i className="btn-login-logo" />
+        </Link >
     );
 };
