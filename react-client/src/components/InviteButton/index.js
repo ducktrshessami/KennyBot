a714d1e5ff418c72cfb95ca7c9da61eb32bfd152
ducktrshessami <jwee761@gmail.com>
2021-04-11 11:53:44 -0700

    deleted invite button

--- react-client/src/components/InviteButton/index.js
+++
@@ -1,8 +0,0 @@
-import { Link } from "react-router-dom";
-import "./InviteButton.css";
-
-export default function InviteButton() {
-    return (
-        <Link className="btn btn-invite" to="/invite" target="_blank">Invite KennyBot to your server</Link>
-    );
-};
--- react-client/src/pages/Home/index.js
+++ react-client/src/pages/Home/index.js
@@ -1,11 +1,9 @@
 import LoginButton from "../../components/LoginButton";
-import InviteButton from "../../components/InviteButton";
 
 export default function Home() {
     return (
         <main>
             <LoginButton />
-            <InviteButton />
         </main>
     );
 };
