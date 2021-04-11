d3a5dc2cf82bee362918c97deeb55d8340ba060c
ducktrshessami <jwee761@gmail.com>
2021-04-11 15:56:23 -0700

    dashboard link in nav

--- react-client/src/components/Navbar/index.js
+++ react-client/src/components/Navbar/index.js
@@ -5,7 +5,13 @@ export default function Navbar() {
     return (
         <header>
             <nav className="nqb-bg">
-                <Link to="/logout" className="logout right">Log Out</Link>
+                <div className="nav-wrapper">
+                    <Link to="/dashboard" className="left dash-link">
+                        <img src={process.env.PUBLIC_URL + "/logo100.png"} alt="Kenny" className="logo" />
+                        <span className="hide-on-small-only">KennyBot</span>
+                    </Link>
+                    <Link to="/logout" className="logout right">Log Out</Link>
+                </div>
             </nav>
         </header>
     );
