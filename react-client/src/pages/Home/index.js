dac5869d102f97b8db14fe6ecca4d2fe3b4fde93
ducktrshessami <jwee761@gmail.com>
2021-04-11 12:07:48 -0700

    authed users are redirected to dashboard from home

--- react-client/src/pages/Home/index.js
+++ react-client/src/pages/Home/index.js
@@ -1,6 +1,16 @@
+import { useHistory } from "react-router-dom";
 import LoginButton from "../../components/LoginButton";
+import API from "../../utils/API";
 
 export default function Home() {
+    const history = useHistory();
+    API.getUser()
+        .then(res => {
+            if (res) {
+                history.push("/dashboard")
+            }
+        })
+        .catch(console.error);
     return (
         <main>
             <LoginButton />
