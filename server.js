55ada099ae0d357ab8736cdcef9cc9699aaeb6fc
ducktrshessami <jwee761@gmail.com>
2021-04-10 22:00:22 -0700

    init api after bot to grab client id from bot

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ const db = require("./models");
 
 db.sequelize.sync({ force: process.env.DB_FORCE && process.env.DB_FORCE.trim().toLowerCase() !== "false" })
     .then(() => {
-        process.bot = require("./bot");
-        process.api = require("./api");
-
         function failed() {
             process.exit();
         }
 
+        process.bot = require("./bot");
+
         process.bot.on("ready", () => {
             process.bot.off("error", failed);
+            process.api = require("./api");
         });
 
         process.bot.on("error", failed);
