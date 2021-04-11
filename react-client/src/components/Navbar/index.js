4dbea74fa15159218d54994f97265722b14df602
ducktrshessami <jwee761@gmail.com>
2021-04-11 14:37:17 -0700

    logout link positioning

--- react-client/src/components/Navbar/index.js
+++ react-client/src/components/Navbar/index.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <header>
             <nav className="nqb-bg">
-                <Link to="/logout">Log Out</Link>
+                <Link to="/logout" className="logout right">Log Out</Link>
             </nav>
         </header>
     );
