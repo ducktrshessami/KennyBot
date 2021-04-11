a2c2ac9977fdaf5ee2530ad504d30dde53c3c121
ducktrshessami <jwee761@gmail.com>
2021-04-11 13:12:31 -0700

    replaced copyright footer with info footer

--- react-client/src/components/Footer/index.js
+++ react-client/src/components/Footer/index.js
@@ -1,8 +1,29 @@
-export default function Footer() {
-    let year = (new Date()).getFullYear();
+import {
+    Link,
+    Route,
+    Switch
+} from "react-router-dom";
+
+export default function Footer(props) {
     return (
         <footer>
-            <span>Copyright ducktrshessami {year}</span>
+            <div className="center row">
+                <div className="col s12 m8 l6 offset-m2 offset-l3 row">
+                    <span className="col s6">
+                        <Switch>
+                            <Route path="/disclaimer">
+                                <Link className="greyple-text" to={props.user ? "/dashboard" : "/"}>{props.user ? "Dashboard" : "Home"}</Link>
+                            </Route>
+                            <Route path="/">
+                                <Link className="greyple-text" to="/disclaimer">Disclaimer</Link>
+                            </Route>
+                        </Switch>
+                    </span>
+                    <span className="col s6">
+                        <a className="greyple-text" href="https://github.com/ducktrshessami/kennybot">GitHub</a>
+                    </span>
+                </div>
+            </div>
         </footer>
     );
 };
