eebda1a41be4e7d4c2216c907056973fc09ceb13
Josh Fish <jwee761@gmail.com>
2021-04-11 23:46:08 -0700

    created ServerCard component

---
+++ react-client/src/components/ServerCard/index.js
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+export default function ServerCard(props) {
+    return (
+        <Link className="card"></Link>
+    );
+};
--- react-client/src/pages/Dashboard/index.js
+++ react-client/src/pages/Dashboard/index.js
@@ -1,9 +1,14 @@
 import { Redirect } from "react-router-dom";
+import ServerCard from "../../components/ServerCard";
+import "./Dashboard.css";
 
 export default function Dashboard(props) {
     return (
         <main>
-            {!props.user ? <Redirect to="/" /> : undefined}
+            <div className="dashboard">
+                {!props.user ? <Redirect to="/" /> : undefined}
+                <h4>Servers</h4>
+            </div>
         </main>
     );
 };
