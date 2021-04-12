bc2952c66e54522807dd722a9ef54d2a2c86bd5b
ducktrshessami <jwee761@gmail.com>
2021-04-11 18:24:20 -0700

    only serve static build if exists

--- api/public.js
+++ api/public.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const router = express.Router();
 const staticDir = path.resolve(__dirname, "..", "react-client", "build");
 
-router.use(express.static(staticDir));
+if (fs.existsSync(staticDir)) {
+    router.use(express.static(staticDir));
 
-router.get("*", function (req, res) {
-    res.redirect("/");
-});
+    router.get("*", function (req, res) {
+        res.redirect("/");
+    });
+}
 
 module.exports = router;
