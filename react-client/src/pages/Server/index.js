97a97e18cd1298260309bb9318e4fc3cb6373332
ducktrshessami <jwee761@gmail.com>
2021-04-12 21:23:16 -0700

    Server info page flow

--- react-client/src/pages/Server/index.js
+++ react-client/src/pages/Server/index.js
@@ -1,10 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import "./Server.css";
+
+const emptyGuild = {
+    name: ""
+};
 
 export default function Server(props) {
+    let guild = emptyGuild;
+    const guildID = (window.location.hash.match(/server\/[0-9]+/i) || [""])[0].match(/[0-9]+/)[0];
+    if (props.guilds) {
+        guild = props.guilds.find(server => server.id === guildID) || {};
+    }
     return (
         <main>
-            <section>
+            {!props.user && props.ready ? <Redirect to="/" /> : undefined}
+            {!guild && props.ready ? <Redirect to="/dashboard" /> : undefined}
+            <section className="server-wrapper">
                 <Link to="/dashboard" className="greyple-bg focus-darken white-text btn">❮ Back</Link>
+                <div className="server-container">
+                    <h4>{guild.name}</h4>
+                </div>
             </section>
         </main>
     );
