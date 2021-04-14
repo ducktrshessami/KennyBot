c7f642de5a21a57d5190682ed81216f8f46c3ee9
ducktrshessami <jwee761@gmail.com>
2021-04-14 11:51:56 -0700

    playlist title styling

--- react-client/src/components/Playlist/SongList.js
+++ react-client/src/components/Playlist/SongList.js
@@ -2,7 +2,7 @@ import Song from "./Song";
 
 export default function SongList(props) {
     return (
-        <ul>
+        <ul className="song-list">
             {props.songs.map(song => <Song key={song.id} {...song} />)}
         </ul>
     );
--- react-client/src/components/Playlist/index.js
+++ react-client/src/components/Playlist/index.js
@@ -7,7 +7,10 @@ export default function Playlist(props) {
     return (
         <li>
             <div className="playlist card dark-kenny-bg">
-                <h5 role="button" className={`playlist-title ${active ? "open" : ""} card-title dark-kenny-bg focus-lighten`} onClick={() => setActive(!active)}>{props.name}</h5>
+                <h5 role="button" className={`playlist-title ${active ? "open " : ""}card-title kenny-bg focus-lighten`} onClick={() => setActive(!active)}>
+                    <span className="playlist-arrow">❯</span>
+                    {props.name}
+                </h5>
                 {active ? <SongList songs={props.Songs} /> : undefined}
             </div>
         </li>
