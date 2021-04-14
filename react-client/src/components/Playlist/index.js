2bc5b646d89a9f51a7ffd766446df41f2c3d01e2
ducktrshessami <jwee761@gmail.com>
2021-04-14 09:57:37 -0700

    playlist title button border styling

--- react-client/src/components/Playlist/index.js
+++ react-client/src/components/Playlist/index.js
@@ -1,10 +1,14 @@
+import { useState } from "react";
+import SongList from "./SongList";
 import "./Playlist.css";
 
 export default function Playlist(props) {
+    const [active, setActive] = useState(false);
     return (
         <li>
             <div className="playlist card dark-kenny-bg">
-                <h5 className="playlist-title card-title dark-kenny-bg focus-lighten">{props.name}</h5>
+                <h5 role="button" className={`playlist-title ${active ? "open" : ""} card-title dark-kenny-bg focus-lighten`} onClick={() => setActive(!active)}>{props.name}</h5>
+                {active ? <SongList songs={props.Songs} /> : undefined}
             </div>
         </li>
     );
