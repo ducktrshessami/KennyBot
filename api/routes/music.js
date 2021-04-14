3d35dfec1959a6a63b4fa6e0317894518e7d882f
ducktrshessami <jwee761@gmail.com>
2021-04-13 21:32:17 -0700

    removed redundant playlist route

--- api/routes/music.js
+++ api/routes/music.js
@@ -19,25 +19,4 @@ module.exports = function (router) {
             res.status(404).end();
         }
     });
-
-    router.get("/api/playlists/:guildId", auth.authCheck, auth.authGuilds, function (req, res) {
-        if (req.authGuilds.find(guild => guild.id === req.params.guildId)) {
-            db.Guild.findByPk(req.params.guildId, {
-                include: {
-                    model: db.Playlist,
-                    include: db.Song
-                }
-            })
-                .then(guild => {
-                    res.status(200).json(guild.Playlists);
-                })
-                .catch(err => {
-                    console.error(err);
-                    res.status(500).end();
-                });
-        }
-        else {
-            res.status(404).end();
-        }
-    });
 };
