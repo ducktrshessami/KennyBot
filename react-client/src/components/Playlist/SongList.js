842964400bcb339c8ff8663eab776827931d1193
ducktrshessami <jwee761@gmail.com>
2021-04-16 10:08:33 -0700

    empty playlist notice

--- react-client/src/components/Playlist/SongList.js
+++ react-client/src/components/Playlist/SongList.js
@@ -1,9 +1,10 @@
 import Song from "./Song";
+import "./SongList.css";
 
 export default function SongList(props) {
     return (
         <ul className="song-list dark-kenny-bg">
-            {props.songs.map(song => <Song key={song.id} {...song} />)}
+            {props.songs.length ? props.songs.map(song => <Song key={song.id} {...song} />) : <h6 className="playlist-empty">This playlist is empty</h6>}
         </ul>
     );
 };
