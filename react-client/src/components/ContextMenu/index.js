b981bda203deb43a1b01d31048391c77ee0eacbe
ducktrshessami <jwee761@gmail.com>
2021-04-17 11:33:46 -0700

    fixed ContextMenu close handling

--- react-client/src/components/ContextMenu/index.js
+++ react-client/src/components/ContextMenu/index.js
@@ -1,23 +1,46 @@
+import { Component, createRef } from "react";
 import Option from "./Option";
 import "./ContextMenu.css";
 
-function findContextParent(element, root) {
-    return element.classList.contains("context-menu") ? element : element === root ? null : findContextParent(element.parentNode, root);
+function isDescendent(child, ancestor) {
+    return child && ancestor ? child === ancestor ? true : child === document.body ? false : isDescendent(child.parentNode, ancestor) : false;
 }
 
-export default function ContextMenu(props) {
-    function outClickListener(event) {
-        event.stopPropagation();
-        if (!findContextParent(event.target, this)) {
-            this.removeEventListener("click", outClickListener);
-            props.close();
+export default class ContextMenu extends Component {
+    state = {
+        ready: false,
+        active: false
+    }
+    ref = createRef()
+    clickHandler = (event) => {
+        let newState = false;
+        if (isDescendent(event.target, this.props.buttonRef.current)) {
+            newState = !this.state.active;
+        }
+        if (newState !== this.state.active) {
+            this.setState({
+                ...this.state,
+                active: newState
+            });
         }
     }
 
-    document.getElementById("root").addEventListener("click", outClickListener);
-    return (
-        <ul className={`context-menu card ${props.className || ""}`.trim()}>
-            {props.options.map(option => <Option key={option.name} className={props.optionClassName} {...option} />)}
-        </ul>
-    );
+    componentDidMount() {
+        if (this.props.buttonRef.current) {
+            document.getElementById("root").addEventListener("click", this.clickHandler);
+            this.setState({ ready: true });
+        }
+    }
+
+    componentWillUnmount() {
+        document.getElementById("root").removeEventListener("click", this.clickHandler);
+    }
+
+    render() {
+        return this.state.ready && this.state.active ? (
+            <ul className={`context-menu card ${this.props.className || ""}`.trim()} ref={this.ref}>
+                {this.props.options.map(option => <Option key={option.name} className={this.props.optionClassName} {...option} />)}
+            </ul>
+        ) : null
+    }
 };
--- react-client/src/components/Playlist/index.js
+++ react-client/src/components/Playlist/index.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { createRef, useState } from "react";
 import ContextMenu from "../ContextMenu";
 import SongList from "./SongList";
 import "./Playlist.css";
 
 export default function Playlist(props) {
+    const menuRef = createRef();
     const [active, setActive] = useState(false);
-    const [menu, setMenu] = useState(false);
     const menuOptions = [
         {
             name: "Edit",
@@ -35,11 +35,11 @@ export default function Playlist(props) {
                         {props.name}
                     </div>
                     <div className="kenny-bg focus-lighten" role="button">▶</div>
-                    <div className="playlist-title-menu kenny-bg focus-lighten" role="button" onClick={() => setMenu(!menu)}>
+                    <div className="playlist-title-menu kenny-bg focus-lighten" role="button" ref={menuRef}>
                         <i className="minimal-text">&nbsp;</i>
                         <i className="kebab-menu" />
                     </div>
-                    {menu ? <ContextMenu optionClassName="kenny-bg focus-lighten" options={menuOptions} close={() => setMenu(false)} /> : undefined}
+                    <ContextMenu optionClassName="kenny-bg focus-lighten" options={menuOptions} buttonRef={menuRef} />
                 </div>
                 {active ? <SongList songs={props.Songs} /> : undefined}
             </div>
