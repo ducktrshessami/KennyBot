b139960d5b0a2567306f0fcaf1787be375351433
ducktrshessami <jwee761@gmail.com>
2021-04-19 13:09:52 -0700

    toast status colors

--- react-client/src/utils/Toast.js
+++ react-client/src/utils/Toast.js
@@ -1,10 +1,18 @@
 import M from "materialize-css";
 
 export default function Toast(message, type = 0) {
+    let wrapperClass = "";
+    switch (type) {
+        case 0: wrapperClass = "greyple-bg"; break;
+        case 1: wrapperClass = "kenny-bg"; break;
+        default:
+    }
     return new Promise((resolve, reject) => {
         M.toast({
             html: `
-                <p>${message}</p>
+                <div class="toast-wrapper ${wrapperClass}">
+                    <h6>${message}</h6>
+                </div>
             `,
             displayLength: 5000,
             activationPercent: 0.66,
