46e42700f873e6748d2fed1819f2959e96391e9b
ducktrshessami <jwee761@gmail.com>
2021-04-21 16:19:16 -0700

    handle client side socket initialization

--- client/src/pages/Server/index.js
+++ client/src/pages/Server/index.js
@@ -4,78 +4,53 @@ import Loading from "../../components/Loading";
 import VoiceChannel from "../../components/VoiceChannel";
 import Playlist from "../../components/Playlist";
 import CreatePlaylist from "../../components/CreatePlaylist";
-import API from "../../utils/API";
+import Socket from "socket.io-client";
 import Toast from "../../utils/Toast";
 import "./Server.css";
 
 export default class Server extends Component {
+    guildID = ((window.location.pathname.match(/server\/[0-9]+/gi) || [""])[0].match(/[0-9]+/g) || [""])[0];
     state = {
         ready: false,
-        failed: false,
-        guild: { name: "" },
-        creating: false
-    }
-
-    refreshComponent() {
-        this.setState({
-            ready: false,
-            failed: false,
-            guild: { name: "" },
-            creating: false
-        }, this.componentDidMount);
+        creating: false,
+        guild: {
+            name: "",
+            state: {}
+        }
     }
 
     componentDidMount() {
-        this.handleUrl()
-            .then(() => this.getGuildFromDb())
-            .catch(console.error);
-    }
+        const socket = Socket({
+            auth: { guildID: this.guildID }
+        });
 
-    componentDidUpdate() {
-        console.log(this.state);
-        console.log(this.props);
-    }
+        socket.on("connect_error", console.error);
+        socket.once("connect_error", () => Toast("Failed to poll live voice state connection", 1));
 
-    handleUrl() {
-        return new Promise((resolve, reject) => {
-            let guildID = ((window.location.pathname.match(/server\/[0-9]+/gi) || [""])[0].match(/[0-9]+/g) || [""])[0];
-            if (this.props.guilds) {
-                let guild = this.props.guilds.find(server => server.id === guildID);
-                if (guild) {
-                    this.setState({
-                        ...this.state,
-                        guild
-                    }, resolve);
+        socket.on("stateInitial", voiceState => {
+            let newState = {
+                ...this.state,
+                ready: true
+            };
+            newState.guild.state = voiceState;
+            this.setState(newState);
+        });
 
-                }
-                else {
-                    this.setState({
-                        ...this.state,
-                        failed: true,
-                        ready: true
-                    }, resolve);
-                }
-            }
+        socket.on("stateUpdate", voiceState => {
+            let newState = { ...this.state };
+            newState.guild.state = voiceState;
+            this.setState(newState);
         });
     }
 
-    getGuildFromDb() {
-        if (!this.state.failed) {
-            return API.getGuildInfo(this.state.guild.id)
-                .then(dbGuild => {
-                    if (dbGuild) {
-                        return new Promise((resolve, reject) => {
-                            this.setState({
-                                ...this.state,
-                                guild: {
-                                    ...this.state.guild,
-                                    ...dbGuild
-                                },
-                                ready: true
-                            })
-                        });
-                    }
-                });
+    componentDidUpdate() {
+        let guild = this.props.guilds.find(guild => guild.id === this.guildID) || {};
+        let name = guild.name || "";
+        console.log(this.state);
+        if (this.state.guild.name !== name) {
+            let newState = { ...this.state };
+            newState.guild.name = name;
+            this.setState(newState);
         }
     }
 
@@ -107,7 +82,7 @@ export default class Server extends Component {
         this.setState({
             ...this.state,
             creating: false
-        }, this.refreshComponent);
+        });
     }
 
     failSucc() {
@@ -118,7 +93,6 @@ export default class Server extends Component {
         return (
             <main>
                 {!this.props.user && this.props.ready ? <Redirect to="/" /> : undefined}
-                {this.state.failed && this.props.ready ? <Redirect to="/dashboard" /> : undefined}
                 <div className="server-wrapper">
                     <Link to="/dashboard" className="greyple-bg focus-darken white-text btn">❮ Back</Link>
                     <div className="server-container row">
@@ -128,7 +102,7 @@ export default class Server extends Component {
                                 {!this.state.ready || !this.props.ready ? <Loading className="server-loader" size="small" /> : undefined}
                             </h4>
                             <br />
-                            {this.state.guild.voice ? <VoiceChannel {...this.state.guild.voice} /> : this.state.ready ? <h6>Not connected to a voice channel</h6> : undefined}
+                            {this.state.guild.state.voice ? <VoiceChannel {...this.state.guild.state.voice} /> : this.state.ready ? <h6>Not connected to a voice channel</h6> : undefined}
                         </section>
                         <section className="col s12 m6 l8">
                             <div className="playlist-header">
@@ -137,7 +111,7 @@ export default class Server extends Component {
                             </div>
                             {this.state.creating ? <CreatePlaylist guildId={this.state.guild.id} onSuccess={() => this.createSucc()} onError={() => this.failSucc()} /> : undefined}
                             <ul className="playlist-wrapper">
-                                {this.state.guild.playlists && this.state.guild.playlists.length ? this.state.guild.playlists.map(playlist => <Playlist key={playlist.id} canPlay={Boolean(this.state.guild.voice)} refreshServer={() => this.refreshComponent()} {...playlist} />) : this.state.ready ? <h6>This server has no playlists</h6> : undefined}
+                                {this.state.guild.state.playlists && this.state.guild.state.playlists.length ? this.state.guild.state.playlists.map(playlist => <Playlist key={playlist.id} canPlay={Boolean(this.state.guild.state.voice)} {...playlist} />) : this.state.ready ? <h6>This server has no playlists</h6> : undefined}
                             </ul>
                         </section>
                     </div>
--- server/ws/listeners/connection.js
+++ server/ws/listeners/connection.js
@@ -4,7 +4,7 @@ module.exports = function (ws) {
     ws.on("connection", function (socket) {
         socket.join(socket.handshake.auth.guildID);
         getNewState(socket.handshake.auth.guildID)
-            .then(state => socket.emit("initial", state))
+            .then(state => socket.emit("stateInitial", state))
             .catch(err => {
                 console.error(err);
                 socket.emit("error", "Failed to get initial state");
