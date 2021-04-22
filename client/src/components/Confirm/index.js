78933f68fe20a2c286874f9dfb9afb8a8c123cd4
ducktrshessami <jwee761@gmail.com>
2021-04-22 11:51:26 -0700

    confirm cancels on clicking outside the box

--- client/src/components/Confirm/index.js
+++ client/src/components/Confirm/index.js
@@ -1,11 +1,22 @@
+import { createRef } from "react";
+import isDescendent from "../../utils/isDescendent";
 import "./Confirm.css";
 
 export default function Confirm(props) {
+    const cardRef = createRef();
+
+    function outClick(event) {
+        event.stopPropagation();
+        if (!isDescendent(event.target, cardRef.current)) {
+            props.onCancel();
+        }
+    }
+
     return (
-        <section>
+        <section onClick={outClick}>
             <div className="confirm-background" />
             <div className="confirm-wrapper row">
-                <div className="confirm-card card nqb-bg col s10 m6 l4 offset-s1 offset-m3 offset-l4">
+                <div className="confirm-card card nqb-bg col s10 m6 l4 offset-s1 offset-m3 offset-l4" ref={cardRef}>
                     <div className="card-content">
                         {props.title ? <div className="card-title">{props.title}</div> : undefined}
                         {props.body ? <p>{props.body}</p> : undefined}
