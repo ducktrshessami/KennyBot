7752f10cf5e254fb7704c12bfa43ebf01757f2e1
ducktrshessami <jwee761@gmail.com>
2021-04-22 15:39:49 -0700

    send index rather than redirect for public page

--- server/http/public.js
+++ server/http/public.js
@@ -4,12 +4,13 @@ const fs = require("fs");
 
 const router = express.Router();
 const staticDir = path.resolve(__dirname, "..", "..", "client", "build");
+const staticIndex = path.join(staticDir, "index.html");
 
 if (fs.existsSync(staticDir)) {
     router.use(express.static(staticDir));
 
     router.get("*", function (req, res) {
-        res.redirect("/");
+        res.status(200).sendFile(staticIndex);
     });
 }
 
