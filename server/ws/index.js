8895c9841667c3d225c5d03d18c01f7e18b13186
ducktrshessami <jwee761@gmail.com>
2021-04-21 21:53:13 -0700

    listners are for sockets, not the io client

--- server/ws/index.js
+++ server/ws/index.js
@@ -1,8 +1,9 @@
 const Socket = require("socket.io");
-const listeners = require("./listeners");
+const socketListeners = require("./socketListeners");
 const session = require("./middleware/session");
 const authCheck = require("./middleware/authCheck");
 const guildCheck = require("./middleware/guildCheck");
+const { getNewState } = require("../../utils/state");
 
 module.exports = function (server) {
     const ws = Socket(server);
@@ -11,7 +12,16 @@ module.exports = function (server) {
     ws.use(authCheck);
     ws.use(guildCheck);
 
-    listeners(ws);
+    ws.on("connection", function (socket) {
+        socket.join(socket.handshake.auth.guildID);
+        getNewState(socket.handshake.auth.guildID)
+            .then(state => socket.emit("stateInitial", state))
+            .catch(err => {
+                console.error(err);
+                socket.emit("error", "Failed to get initial state");
+            });
+        socketListeners(socket);
+    });
 
     return ws;
 };
--- server/ws/socketListeners/index.js
+++ server/ws/socketListeners/index.js
@@ -3,14 +3,14 @@ const path = require("path");
 
 const basename = path.basename(__filename);
 
-module.exports = function (ws) {
+module.exports = function (socket) {
     fs
         .readdirSync(__dirname)
         .filter(file => {
             return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
         })
         .forEach(file => {
-            require(path.join(__dirname, file))(ws);
+            require(path.join(__dirname, file))(socket);
         });
-    return ws;
+    return socket;
 };
