319ec120a7298672b0d48de9f025e1c857e2e1e1
ducktrshessami <jwee761@gmail.com>
2021-04-22 11:34:08 -0700

    confirmation dialogue component

---
+++ client/src/components/Confirm/index.js
@@ -0,0 +1,21 @@
+import "./Confirm.css";
+
+export default function Confirm(props) {
+    return (
+        <section>
+            <div className="confirm-background" />
+            <div className="confirm-wrapper row">
+                <div className="confirm-card card nqb-bg col s10 m6 l4 offset-s1 offset-m3 offset-l4">
+                    <div className="card-content">
+                        {props.title ? <div className="card-title">{props.title}</div> : undefined}
+                        {props.body ? <p>{props.body}</p> : undefined}
+                    </div>
+                    <div className="card-action">
+                        <button className="confirm-button btn greyple-bg focus-lighten right">Cancel</button>
+                        <button className="confirm-button btn kenny-bg focus-lighten right">Ok</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
