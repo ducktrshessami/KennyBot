ce6f33d9a20bc27cfbf52e1d106a6c242b26dafc
ducktrshessami <jwee761@gmail.com>
2021-05-07 17:20:26 -0700

    soundcloud playlist parsing

--- utils/audio.js
+++ utils/audio.js
@@ -1,4 +1,5 @@
 const ytdl = require("ytdl-core");
+const ytpl = require("ytpl");
 const scdl = require("scdl-core");
 const config = require("../config/audio.json");
 
@@ -6,6 +7,8 @@ module.exports = controller;
 module.exports.getSource = getSource;
 module.exports.getTitle = getTitle;
 module.exports.formatUrl = formatUrl;
+module.exports.parsePlaylist = parsePlaylist;
+module.exports.getPlaylistSource = getPlaylistSource;
 
 scdl.setClientID(process.env.SCDL_CLIENTID || config.scdl.client_id);
 scdl.setOauthToken(process.env.SCDL_TOKEN || config.scdl.oauth_token);
@@ -38,6 +41,18 @@ function getSource(url) {
     }
 }
 
+async function getPlaylistSource(url) {
+    try {
+        await ytpl.getPlaylistID(url);
+        return "youtube";
+    }
+    catch {
+        if (scdl.playlist.validateURL(url)) {
+            return "soundcloud";
+        }
+    }
+}
+
 function getTitle(url) {
     return new Promise((resolve, reject) => {
         let source = getSource(url);
@@ -61,7 +76,39 @@ function getTitle(url) {
 function formatUrl(url) {
     let source = getSource(url);
     switch (source) {
-        case "youtube": return `https://www.youtube.com/watch?v=${ytdl.getURLVideoID(url)}`;
+        case "youtube": return `https://youtube.com/watch?v=${ytdl.getURLVideoID(url)}`;
         case "soundcloud": return scdl.getPermalinkURL(url);
+        default:
     }
+}
+
+function parsePlaylist(url) {
+    return getPlaylistSource(url)
+        .then(source => {
+            switch (source) {
+                case "youtube": return parseYoutube(url);
+                case "soundcloud": return parseSoundcloud(url);
+                default:
+            }
+        });
+}
+
+function parseYoutube(url) {
+
+}
+
+function parseSoundcloud(url) {
+    return scdl.playlist.getInfo(url)
+        .then(info => {
+            if (info.tracks) {
+                return info.tracks.map(track => ({
+                    title: `${track.user.username} - ${track.title}`,
+                    url: track.permalink_url,
+                    source: "soundcloud"
+                }));
+            }
+            else {
+                return [];
+            }
+        });
 }
