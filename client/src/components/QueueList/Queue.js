90629cce6702ac4ec279df5ec85068c598bd08f2
ducktrshessami <jwee761@gmail.com>
2021-05-09 20:35:44 -0700

    queue button styling

--- client/src/components/QueueList/Queue.js
+++ client/src/components/QueueList/Queue.js
@@ -1,6 +1,12 @@
 export default function Queue(props) {
     console.log(props);
     return (
-        <li>{props.title}</li>
+        <li className="queued-song">
+            <i role="button" className="queue-draggable-icon" />
+            <div className="queued-title center">
+                <a href={props.url} target="_blank" rel="noreferrer" className="greyple-text">{props.title} <i /></a>
+            </div>
+            <i role="button" className="queue-remove-icon" />
+        </li>
     );
 }
--- client/src/components/QueueList/index.js
+++ client/src/components/QueueList/index.js
@@ -1,11 +1,13 @@
 import Queue from "./Queue";
+import "./QueueList.css";
 
 export default function QueueList(props) {
     console.log(props);
     return (
-        <section className="nqb-bg">
+        <section className="queue-list nqb-bg">
+            <h5 className="queue-title">Queued:</h5>
             <ul>
-                {props.queue.map(item => <Queue key={item.id} id={item.id} {...item.Song} />)}
+                {props.queue.map(item => <Queue key={item.id} id={item.id} title={item.Song.title} url={item.Song.url} />)}
             </ul>
         </section>
     );
