89e36893916758d9f7249a0af26dc653396386e9
ducktrshessami <jwee761@gmail.com>
2021-05-09 22:43:09 -0700

    limited queue dragging

--- client/src/components/QueueList/Queue.js
+++ client/src/components/QueueList/Queue.js
@@ -1,7 +1,19 @@
 import { useState } from "react";
+import { useDrag } from "react-use-gesture";
+import { useSpring, animated } from "react-spring";
 
 export default function Queue(props) {
     const [ghost, setGhost] = useState(false);
+    const [{ y }, spring] = useSpring(() => ({ y: 0 }));
+    const dragBind = useDrag(state => {
+        let listRect = props.getListRect();
+        if (state.xy[1] > listRect.top && state.xy[1] < listRect.bottom) {
+            spring.start({ y: state.movement[1] });
+        }
+        if (!state.down) {
+            //done sorting
+        }
+    });
 
     function dequeue() {
         if (props.socket) {
@@ -13,12 +25,12 @@ export default function Queue(props) {
     console.log(props);
 
     return ghost ? null : (
-        <li className="queued-song">
-            <i role="button" className="queue-draggable-icon queue-button-icon" />
+        <animated.li className="queued-song" style={{ y }}>
+            <i role="button" className="queue-draggable-icon queue-button-icon" {...dragBind()} />
             <div className="queued-title center">
                 <a href={props.url} target="_blank" rel="noreferrer" className="greyple-text">{props.title} <i /></a>
             </div>
             <i role="button" className="queue-remove-icon queue-button-icon" onClick={dequeue} />
-        </li>
+        </animated.li>
     );
 }
--- client/src/components/QueueList/index.js
+++ client/src/components/QueueList/index.js
@@ -1,13 +1,21 @@
+import { createRef } from "react";
 import Queue from "./Queue";
 import "./QueueList.css";
 
 export default function QueueList(props) {
+    const listRef = createRef();
+
+    function getListRect() {
+        return listRef.current.getBoundingClientRect();
+    }
+
     console.log(props);
+
     return (
         <section>
             <h5 className="queue-title nqb-bg">Queued:</h5>
-            <ul>
-                {props.queue.map(item => <Queue key={item.id} id={item.id} socket={props.socket} title={item.Song.title} url={item.Song.url} />)}
+            <ul ref={listRef}>
+                {props.queue.map(item => <Queue key={item.id} id={item.id} socket={props.socket} title={item.Song.title} url={item.Song.url} getListRect={getListRect} />)}
             </ul>
         </section>
     );
