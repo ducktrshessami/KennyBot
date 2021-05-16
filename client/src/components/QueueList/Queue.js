affa734a19ef7466cee41bc831b4c802f7ecc15c
ducktrshessami <jwee761@gmail.com>
2021-05-16 12:49:35 -0700

    structure for cross-component drag

--- client/src/components/QueueList/Queue.js
+++ client/src/components/QueueList/Queue.js
@@ -1,19 +1,9 @@
-import { useState } from "react";
-import { useDrag } from "react-use-gesture";
+import { useEffect, useState } from "react";
 import { useSpring, animated } from "react-spring";
 
 export default function Queue(props) {
     const [ghost, setGhost] = useState(false);
-    const [{ y }, spring] = useSpring(() => ({ y: 0 }));
-    const dragBind = useDrag(state => {
-        let listRect = props.getListRect();
-        if (state.xy[1] > listRect.top && state.xy[1] < listRect.bottom) {
-            spring.start({ y: state.movement[1] });
-        }
-        if (!state.down) {
-            //done sorting
-        }
-    });
+    const [coords, spring] = useSpring(() => ({ y: 0 }));
 
     function dequeue() {
         if (props.socket) {
@@ -22,11 +12,13 @@ export default function Queue(props) {
         }
     }
 
-    console.log(props);
+    useEffect(() => {
+        spring.start({ y: props.y });
+    }, [spring, props.y]);
 
     return ghost ? null : (
-        <animated.li className="queued-song" style={{ y }}>
-            <i role="button" className="queue-draggable-icon queue-button-icon" {...dragBind()} />
+        <animated.li data-id={props.id} className={`queued-song ${props.active ? "dragging" : ""}`.trim()} style={coords}>
+            <i role="button" className="queue-draggable-icon queue-button-icon" {...props.dragBinder()} />
             <div className="queued-title center">
                 <a href={props.url} target="_blank" rel="noreferrer" className="greyple-text">{props.title} <i /></a>
             </div>
--- client/src/components/QueueList/index.js
+++ client/src/components/QueueList/index.js
@@ -1,21 +1,55 @@
-import { createRef } from "react";
+import { createRef, useEffect, useState } from "react";
+import { useDrag } from "react-use-gesture";
 import Queue from "./Queue";
 import "./QueueList.css";
 
+function queueFromDrag(child) {
+    return child.classList.contains("queued-song") ? child : child.parentNode ? child.parentNode : null;
+}
+
 export default function QueueList(props) {
     const listRef = createRef();
+    const [active, setActive] = useState(null);
+    const [y, setY] = useState(0);
+    const [bounds, setBounds] = useState(null);
+    const dragBinder = useDrag(state => {
+        let activeItem = queueFromDrag(state.event.target);
+        if (!active && activeItem) {
+            setActive({
+                id: activeItem.dataset.id,
+                height: activeItem.getBoundingClientRect().height
+            });
+        }
+        if (state.down) {
+            if (bounds && state.xy[1] > bounds.top && state.xy[1] < bounds.bottom) {
+                setY(state.movement[1]);
+            }
+        }
+        else {
+            setActive(null);
+            setY(0);
+        }
+    }, { axis: "y" });
+    const rendered = props.queue.map(item => {
+        let activeItem = active && item.id === active.id;
+        return <Queue key={item.id} id={item.id} socket={props.socket} title={item.Song.title} url={item.Song.url} dragBinder={dragBinder} active={activeItem} y={activeItem ? y : 0} />
+    });
 
-    function getListRect() {
-        return listRef.current.getBoundingClientRect();
-    }
-
-    console.log(props);
+    useEffect(() => {
+        if (listRef.current) {
+            let prev = bounds || {};
+            let next = listRef.current.getBoundingClientRect();
+            if (prev.top !== next.top || prev.bottom !== next.bottom) {
+                setBounds(next);
+            }
+        }
+    }, [listRef, bounds]);
 
     return (
         <section>
             <h5 className="queue-title nqb-bg">Queued:</h5>
             <ul ref={listRef}>
-                {props.queue.map(item => <Queue key={item.id} id={item.id} socket={props.socket} title={item.Song.title} url={item.Song.url} getListRect={getListRect} />)}
+                {rendered}
             </ul>
         </section>
     );
