42432c76a9fe419cdcd2857c64cb55f084fa35e6
ducktrshessami <jwee761@gmail.com>
2021-05-18 17:01:53 -0700

    queue swipe visibility

---
+++ client/src/components/QueueList/Mobile/Queue.js
@@ -0,0 +1,3 @@
+export default function Queue(props) {
+    return null;
+};
--- client/src/components/QueueList/Mobile/index.js
+++ client/src/components/QueueList/Mobile/index.js
@@ -1,3 +1,37 @@
+import { useEffect, useState } from "react";
+import { animated, useSpring } from "react-spring";
+import { useDrag } from "react-use-gesture";
+import "./MobileQueueList.css"
+
 export default function Mobile(props) {
-    return null;
+    const [visible, setVisible] = useState(false);
+    const [coords, spring] = useSpring(() => ({ x: visible ? 0 : -window.innerWidth }));
+
+    useDrag(state => {
+        if (state.down) {
+            spring.start({ x: visible ? Math.min(0, state.movement[0]) : Math.max(-window.innerWidth, state.movement[0] - window.innerWidth) });
+        }
+        else {
+            switch (state.swipe[0]) {
+                case 1: setVisible(true); break;
+                case -1: setVisible(false); break;
+                default: setVisible(coords.x.get() > (-window.innerWidth / 2)); break;
+            }
+            spring.start({ x: visible ? 0 : -window.innerWidth });
+        }
+    }, {
+        axis: "x",
+        useTouch: true,
+        filterTaps: true,
+        swipeDuration: Infinity,
+        experimental_preventWindowScrollY: true,
+        domTarget: document.getElementById("root")
+    });
+    useEffect(() => {
+        spring.start({ x: visible ? 0 : -window.innerWidth });
+    });
+
+    return (
+        <animated.article className="mobile-queue kenny-bg hide-on-med-and-up" style={coords}>asdf</animated.article>
+    );
 };
