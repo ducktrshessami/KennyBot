77df9378c909ac7b723dfc487599230675e4489c
ducktrshessami <jwee761@gmail.com>
2021-05-18 20:48:45 -0700

    better queue visibility drag bounds

--- client/src/components/QueueList/Mobile/index.js
+++ client/src/components/QueueList/Mobile/index.js
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
 import { animated, useSpring } from "react-spring";
 import { useDrag } from "react-use-gesture";
+import Queue from "./Queue";
 import "./MobileQueueList.css"
 
 export default function Mobile(props) {
     const [visible, setVisible] = useState(false);
     const [coords, spring] = useSpring(() => ({ x: visible ? 0 : -window.innerWidth }));
+    const dragBind = useDrag(state => {
+
+    });
 
     useDrag(state => {
         if (state.down) {
-            spring.start({ x: visible ? Math.min(0, state.movement[0]) : Math.max(-window.innerWidth, state.movement[0] - window.innerWidth) });
+            spring.start({ x: visible ? state.movement[0] : state.movement[0] - window.innerWidth });
         }
         else {
             switch (state.swipe[0]) {
@@ -21,6 +25,10 @@ export default function Mobile(props) {
         }
     }, {
         axis: "x",
+        bounds: {
+            left: -window.innerWidth,
+            right: window.innerWidth
+        },
         useTouch: true,
         filterTaps: true,
         swipeDuration: Infinity,
@@ -32,6 +40,11 @@ export default function Mobile(props) {
     });
 
     return (
-        <animated.article className="mobile-queue kenny-bg hide-on-med-and-up" style={coords}>asdf</animated.article>
+        <animated.article className="mobile-queue nqb-bg hide-on-med-and-up" style={coords}>
+            <h4 className="queue-title center">Queue</h4>
+            <ul>
+                {props.queue.map(item => <Queue />)}
+            </ul>
+        </animated.article>
     );
 };
