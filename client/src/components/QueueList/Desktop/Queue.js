46ff94513ab832fe4a43885721db1c6f3f5f1852
ducktrshessami <jwee761@gmail.com>
2021-05-22 15:30:05 -0700

    removed unused i tag in queued song title

--- client/src/components/QueueList/Desktop/Queue.js
+++ client/src/components/QueueList/Desktop/Queue.js
@@ -15,7 +15,7 @@ export default function Queue(props) {
         <animated.li className={`queued-song ${props.active ? "dragging" : ""}`.trim()} style={props.style}>
             <i role="button" className="queue-draggable-icon queue-button-icon" ref={props.dragRef} {...props.dragBinder()} />
             <div className="queued-title center">
-                <a href={props.url} target="_blank" rel="noreferrer" className="greyple-text">{props.title} <i /></a>
+                <a href={props.url} target="_blank" rel="noreferrer" className="greyple-text">{props.title}</a>
             </div>
             <i role="button" className="queue-remove-icon queue-button-icon" onClick={dequeue} />
         </animated.li>
