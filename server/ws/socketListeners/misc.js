e9479491f555151f1e10f08e3c9032445006d489
ducktrshessami <jwee761@gmail.com>
2021-05-25 14:21:36 -0700

    misc action logging

--- server/ws/socketListeners/misc.js
+++ server/ws/socketListeners/misc.js
@@ -1,8 +1,10 @@
 const music = require("../../../utils/music");
+const { logAction } = require("../../../utils/user");
 
 module.exports = function (socket) {
     socket.on("volumeChange", function (volume) {
         music.changeVolume(socket.handshake.auth.guildID, volume)
+            .then(() => logAction(socket.handshake.auth.userID, socket.handshake.auth.guildID, `Changed volume to ${volume}`))
             .catch(err => {
                 console.error(err);
                 socket.emit("error", err);
@@ -11,6 +13,7 @@ module.exports = function (socket) {
 
     socket.on("shuffleChange", function (shuffle) {
         music.setShuffle(socket.handshake.auth.guildID, shuffle)
+            .then(() => logAction(socket.handshake.auth.userID, socket.handshake.auth.guildID, `Toggled shuffle ${shuffle ? "on" : "off"}`))
             .catch(err => {
                 console.error(err);
                 socket.emit("error", err);
@@ -18,10 +21,20 @@ module.exports = function (socket) {
     });
 
     socket.on("repeatChange", function (repeat) {
-        music.setRepeat(socket.handshake.auth.guildID, repeat)
-            .catch(err => {
-                console.error(err);
-                socket.emit("error", err);
-            });
+        let actionMessage;
+        switch (repeat) {
+            case 0: actionMessage = "Turned off repeat"; break;
+            case 1: actionMessage = "Turned on repeat one"; break;
+            case 2: actionMessage = "Turned on repeat all"; break;
+            default:
+        }
+        if (actionMessage) {
+            music.setRepeat(socket.handshake.auth.guildID, repeat)
+                .then(() => logAction(socket.handshake.auth.userID, socket.handshake.auth.guildID, actionMessage))
+                .catch(err => {
+                    console.error(err);
+                    socket.emit("error", err);
+                });
+        }
     });
 };
