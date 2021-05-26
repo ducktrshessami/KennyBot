ca6d01a1baf86e1bc3054867aa6ba98a2c632786
ducktrshessami <jwee761@gmail.com>
2021-05-26 13:26:59 -0700

    client audit log redirects

--- client/src/pages/Audit/index.js
+++ client/src/pages/Audit/index.js
@@ -1,5 +1,7 @@
 import { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
 import API from "../../utils/API";
+import "./Audit.css";
 
 export default class Audit extends Component {
     guildID = ((window.location.pathname.match(/audit\/[0-9]+/gi) || [""])[0].match(/[0-9]+/g) || [""])[0];
@@ -16,7 +18,12 @@ export default class Audit extends Component {
 
     render() {
         return (
-            <main></main>
+            <main>
+                {!this.props.user && this.props.ready ? <Redirect to="/" /> : undefined}
+                <div className="audit-wrapper">
+                    <Link to={`/server/${this.guildID}`} className="greyple-bg focus-darken white-text btn">❮ Back</Link>
+                </div>
+            </main>
         );
     }
 };
