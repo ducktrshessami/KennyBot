fce07002409664a8689200e9b4289e8f6008363a
ducktrshessami <jwee761@gmail.com>
2021-06-05 16:51:33 -0700

    properly aligned filter item with box

--- client/src/components/FilterDropdown/FilterItem.js
+++ client/src/components/FilterDropdown/FilterItem.js
@@ -3,9 +3,9 @@ import { forwardRef } from "react";
 export default forwardRef(function FilterItem(props, ref) {
     console.log(props);
     return (
-        <li className="filter-item" ref={ref}>
+        <li className="filter-item" role="button" ref={ref} onClick={props.onClick}>
             <img className="filter-image" alt={`${props.primary} icon`} src={props.image} />
-            <div>{props.primary}<span className="greyple-text">{props.secondary}</span></div>
+            <div className="filter-text">{props.primary}<span className="greyple-text">{props.secondary}</span></div>
         </li>
     );
 });
