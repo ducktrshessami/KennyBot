3f3391b70f3d6e453ccdf270ea6b8ffdde238bcc
ducktrshessami <jwee761@gmail.com>
2021-06-05 15:56:10 -0700

    user to filter item mapping

--- client/src/components/FilterDropdown/index.js
+++ client/src/components/FilterDropdown/index.js
@@ -1,9 +1,34 @@
 import { forwardRef, useState } from "react";
+import FilterItem from "./FilterItem";
+import userIcon from "../../images/user-filter.png";
+import actionIcon from "../../images/action-filter.png";
 import "./FilterDropdown.css";
+
+const actionList = [
+
+];
 
 export default forwardRef(function FilterDropdown(props, ref) {
     const [search, setSearch] = useState("");
+    const items = [
+        {
+            primary: `All ${props.users ? "Users" : "Actions"}`,
+            secondary: "",
+            image: props.users ? userIcon : actionIcon
+        },
+        ...(props.users ? props.users.map(user => ({
+            primary: user.username,
+            secondary: `#${user.discriminator}`,
+            image: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`
+        })) : actionList)
+    ]
+        .filter(item => `${item.primary}${item.secondary}`.includes(search));
     return (
-        <div className={`filter-dropdown dbnb-bg ${props.className ? props.className : ""}`.trim()} ref={ref}></div>
+        <div className={`filter-dropdown dbnb-bg ${props.className ? props.className : ""}`.trim()} ref={ref}>
+            <input className="filter-search dtnqb-bg white-text browser-default" type="text" placeholder={`Search ${props.users ? "Members" : "Actions"}`} onChange={event => setSearch(event.target.value.trim())} />
+            <ul>
+                {items.map((item, i) => <FilterItem key={i} {...item} />)}
+            </ul>
+        </div>
     );
 });
