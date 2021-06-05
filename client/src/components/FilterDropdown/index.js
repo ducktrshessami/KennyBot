d4e74b63e39110e50986161eb4ba4f96131fd053
ducktrshessami <jwee761@gmail.com>
2021-06-05 16:54:34 -0700

    filter selecting

--- client/src/components/FilterDropdown/index.js
+++ client/src/components/FilterDropdown/index.js
@@ -1,5 +1,6 @@
 import { createRef, forwardRef, useState } from "react";
 import FilterItem from "./FilterItem";
+import isDescendent from "../../utils/isDescendent";
 import userIcon from "../../images/user-filter.png";
 import actionIcon from "../../images/action-filter.png";
 import "./FilterDropdown.css";
@@ -27,11 +28,18 @@ export default forwardRef(function FilterDropdown(props, ref) {
         .filter(item => `${item.primary}${item.secondary}`.includes(search));
     const itemRefs = items.map(() => createRef());
 
+    function selectFilter(event) {
+        let i = itemRefs.findIndex(ref => isDescendent(event.target, ref.current));
+        if (i !== -1) {
+            props.select(items[i].value);
+        }
+    }
+
     return (
         <div className={`filter-dropdown dbnb-bg ${props.className ? props.className : ""}`.trim()} ref={ref}>
             <input className="filter-search dtnqb-bg white-text browser-default" type="text" placeholder={`Search ${props.users ? "Members" : "Actions"}`} onChange={event => setSearch(event.target.value.trim())} />
             <ul>
-                {items.map((item, i) => <FilterItem key={i} {...item} ref={itemRefs[i]} />)}
+                {items.map((item, i) => <FilterItem key={item.value} {...item} ref={itemRefs[i]} onClick={selectFilter} />)}
             </ul>
         </div>
     );
