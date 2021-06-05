6f501a9d37590b8246482fd5a3dc266a263924e7
ducktrshessami <jwee761@gmail.com>
2021-06-04 21:17:34 -0700

    default emoji response doesn't trigger if self

--- bot/responses/default.js
+++ bot/responses/default.js
@@ -1,6 +1,8 @@
 const { Response, utils } = require("discord-bot");
 
-module.exports = new Response(["kenny"], undefined, undefined, function (message) {
+module.exports = new Response(["kenny"], undefined, function (message, trigger) {
+    return message.author.id !== this.client.user.id && trigger.every(tr => message.content.toLowerCase().includes(tr.toLowerCase().trim()));
+}, function (message) {
     utils.sendVerbose(message.channel, this.client.emojis.cache.random().toString())
         .catch(console.error);
 });
