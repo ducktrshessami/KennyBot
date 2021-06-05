12ce86926a667749d72895575bd4508ddbd8669e
ducktrshessami <jwee761@gmail.com>
2021-06-05 16:19:30 -0700

    filter dropdown styling

--- client/src/components/FilterDropdown/FilterItem.js
+++ client/src/components/FilterDropdown/FilterItem.js
@@ -3,6 +3,8 @@ import { forwardRef } from "react";
 export default forwardRef(function FilterItem(props, ref) {
     console.log(props);
     return (
-        <li ref={ref}></li>
+        <li className="filter-item" ref={ref}>
+            <div>{props.primary}<span className="greyple-text">{props.secondary}</span></div>
+        </li>
     );
 });
