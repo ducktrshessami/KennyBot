be44242dbb1f18acfe1319b1870d36e89e3a3926
ducktrshessami <jwee761@gmail.com>
2021-06-05 16:28:06 -0700

    image alt text

--- client/src/components/FilterDropdown/FilterItem.js
+++ client/src/components/FilterDropdown/FilterItem.js
@@ -4,6 +4,7 @@ export default forwardRef(function FilterItem(props, ref) {
     console.log(props);
     return (
         <li className="filter-item" ref={ref}>
+            <img className="filter-image" alt={`${props.primary} icon`} src={props.image} />
             <div>{props.primary}<span className="greyple-text">{props.secondary}</span></div>
         </li>
     );
