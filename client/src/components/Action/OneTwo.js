6760c0789d64c5db93630d7f955865fac700c1b6
ducktrshessami <jwee761@gmail.com>
2021-06-06 15:13:44 -0700

    OneTwo style of action element

---
+++ client/src/components/Action/OneTwo.js
@@ -0,0 +1,7 @@
+export default function OneTwo(props) {
+    return (
+        <div className="action-text">
+            <span className="greyple-text">{props.values[0]}</span> {props.values[1]}
+        </div>
+    );
+};
--- client/src/components/Action/index.js
+++ client/src/components/Action/index.js
@@ -1,33 +1,33 @@
+import OneTwo from "./OneTwo";
+import Repeat from "./Repeat";
 import "./Action.css";
 
 export default function Action(props) {
-    const date = new Date(props.createdAt);
-    var header;
-    console.log(props);
+    const timestamp = new Date(props.createdAt);
+    var actionElements;
     switch (props.code) {
-        case 0: header = `paused ${props.vars[0]}`; break;
-        case 1: header = `unpaused ${props.vars[0]}`; break;
-        case 2: header = `skipped ${props.vars[0]}`; break;
-        case 3: header = `set the volume to ${props.vars[0]}`; break;
-        case 4: header = `toggled shuffle ${props.vars[0]}`; break;
-        case 5:
-            switch (props.vars[0]) {
-                case "0": header = "turned off repeat"; break;
-                case "1": header = "turned on repeat one"; break;
-                case "2": header = "turned on repeat all"; break;
-                default:
-            }
-            break;
-        case 6: header = `played song: ${props.vars[0]}`; break;
-        case 7: header = `played playlist: ${props.vars[0]}`; break;
-        case 8: header = `shuffle played playlist: ${props.vars[0]}`; break;
-        case 9: header = `queued ${props.vars[0]}`; break;
-        case 10: header = `dequeued ${props.vars.length} song${props.vars > 1 ? "s" : ""}`; break;
-        case 11: header = `created playlist: ${props.vars[0]}`; break;
-        case 12: header = `renamed ${props.vars[0]} to ${props.vars[1]}`; break;
-        case 13: header = `deleted playlist: ${props.vars[0]}`; break;
-        case 14: header = `added ${props.vars.length - 1} song${props.vars.length > 2 ? "s" : ""} to ${props.vars[0]}`; break;
-        case 15: header = `deleted ${props.vars[1]} from ${props.vars[0]}`; break;
+        case 0: actionElements = <OneTwo values={["paused ", props.vars[0]]} />; break;
+        case 1: actionElements = <OneTwo values={["unpaused ", props.vars[0]]} />; break;
+        case 2: actionElements = <OneTwo values={["skipped ", props.vars[0]]} />; break;
+        case 3: actionElements = <OneTwo values={["set the volume to ", props.vars[0]]} />; break;
+        case 4: actionElements = <OneTwo values={["toggled shuffle ", props.vars[0]]} />; break;
+        case 5: actionElements = <Repeat value={props.vars[0]} />; break;
+        //     switch (props.vars[0]) {
+        //         case "0": actionElements = [<span className="greyple-text">turned </span>, "off", <span className="greyple-text"> repeat</span>]; break;
+        //         case "1": actionElements = [<span className="greyple-text">turned </span>, "on", <span className="greyple-text"> repeat </span>, "one"]; break;
+        //         case "2": actionElements = [<span className="greyple-text">turned </span>, "on", <span className="greyple-text"> repeat </span>, "all"]; break;
+        //         default:
+        //     }
+        case 6: actionElements = <OneTwo values={["played song: ", props.vars[0]]} />; break;
+        case 7: actionElements = <OneTwo values={["played playlist: ", props.vars[0]]} />; break;
+        case 8: actionElements = <OneTwo values={["shuffle played playlist: ", props.vars[0]]} />; break;
+        case 9: actionElements = <OneTwo values={["queued ", props.vars[0]]} />; break;
+        case 10: actionElements = `dequeued ${props.vars.length} song${props.vars > 1 ? "s" : ""}`; break;
+        case 11: actionElements = <OneTwo values={["created playlist: ", props.vars[0]]} />; break;
+        case 12: actionElements = `renamed ${props.vars[0]} to ${props.vars[1]}`; break;
+        case 13: actionElements = <OneTwo values={["deleted playlist: ", props.vars[0]]} />; break;
+        case 14: actionElements = `added ${props.vars.length - 1} song${props.vars.length > 2 ? "s" : ""} to ${props.vars[0]}`; break;
+        case 15: actionElements = `deleted ${props.vars[1]} from ${props.vars[0]}`; break;
         default:
     }
     return (
@@ -36,9 +36,9 @@ export default function Action(props) {
             <img className="action-avatar" alt={`${props.User.username}'s avatar`} src={`https://cdn.discordapp.com/avatars/${props.User.id}/${props.User.avatar}.png`} />
             <div className="action-info">
                 <div className="action-details">
-                    {props.User.username}<span className="greyple-text">#{props.User.discriminator}</span> {header}
+                    {props.User.username}<span className="action-discriminator">#{props.User.discriminator}</span> {actionElements}
                 </div>
-                <div className="action-timestamp greyple-text">{date.toLocaleDateString()}</div>
+                <div className="action-timestamp">{timestamp.toLocaleDateString()} {timestamp.toLocaleTimeString()}</div>
             </div>
         </li>
     );
