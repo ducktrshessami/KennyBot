cc4fe708a65937c9402d5aed706ff1ede8a04b1d
ducktrshessami <jwee761@gmail.com>
2021-06-06 15:44:26 -0700

    onetwo spacing handled externally

--- client/src/components/Action/OneTwo.js
+++ client/src/components/Action/OneTwo.js
@@ -1,7 +1,8 @@
 export default function OneTwo(props) {
     return (
         <div className="action-text">
-            <span className="greyple-text">{props.values[0]}</span> {props.values[1]}
+            <span className="greyple-text">{props.values[0]}</span>
+            {props.values[1]}
         </div>
     );
 };
