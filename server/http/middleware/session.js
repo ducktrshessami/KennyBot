750f70a084d904c0b7dd362cb596b7fb41d9dfb3
ducktrshessami <jwee761@gmail.com>
2021-06-08 16:00:13 -0700

    set up SequelizeStore for session middleware

--- server/http/middleware/session.js
+++ server/http/middleware/session.js
@@ -1,11 +1,19 @@
 const session = require("express-session");
-const MemoryStore = require("memorystore")(session);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const db = require("../../../models");
 
+const store = new SequelizeStore({
+    db: db.sequelize,
+    checkExpirationInterval: 86400000
+});
 const middleware = session({
     secret: process.env.API_SECRET,
-    store: new MemoryStore({ checkPeriod: 86400000 }),
+    store: store,
     resave: true,
     saveUninitialized: true
 });
+
+store.sync({ force: process.env.DB_FORCE && process.env.DB_FORCE.trim().toLowerCase() !== "false" })
+    .catch(console.error);
 
 module.exports = middleware;
