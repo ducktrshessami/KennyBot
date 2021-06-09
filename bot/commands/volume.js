26e6ca3e392a27078d7a5cb1a1e79d8bc72e7952
ducktrshessami <jwee761@gmail.com>
2021-06-09 15:32:03 -0700

    volume command can display current volume

--- bot/commands/volume.js
+++ bot/commands/volume.js
@@ -1,5 +1,6 @@
 const { Command, utils } = require("discord-bot");
 const { changeVolume } = require("../../utils/music");
+const { getNewState } = require("../../utils/state");
 
 module.exports = new Command("volume", function (message, args) {
     let vol = Number(args[1]);
@@ -9,12 +10,18 @@ module.exports = new Command("volume", function (message, args) {
             .catch(console.error);
     }
     else {
-        utils.sendVerbose(message.channel, `${message.author}\n\`${this.usage}\`\n${this.subtitle}`)
-            .catch(console.error);
+        if (args[1]) {
+            utils.sendVerbose(message.channel, `${message.author}\n\`${this.usage}\`\n${this.subtitle}`)
+                .catch(console.error);
+        }
+        else {
+            getNewState(message.guild.id)
+                .then(({ volume }) => utils.sendVerbose(message.channel, `Current volume: \`${volume}\``));
+        }
     }
 }, {
     aliases: ["vol"],
-    usage: "@kennybot volume <value>",
-    description: "Change the volume of the music player",
+    usage: "@kennybot volume [value]",
+    description: "Display or change the volume of the music player",
     subtitle: "Value is truncated to range from `0` to `1.5`"
 });
