66bbc757c0b1b1daca0df66558a3826381d8dbf4
ducktrshessami <jwee761@gmail.com>
2021-06-08 21:19:21 -0700

    suspense loader page

---
+++ client/src/pages/Loader/index.js
@@ -0,0 +1,10 @@
+import Loading from "../../components/Loading";
+import "./Loader.css";
+
+export default function Loader() {
+    return (
+        <main>
+            <Loading className="loading-page" size="big" />
+        </main>
+    )
+};
