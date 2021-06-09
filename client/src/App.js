967fcb38be00aee8f032282fdacb7814d98ae91a
ducktrshessami <jwee761@gmail.com>
2021-06-08 21:19:43 -0700

    route based lazy loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,26 @@
-import { Component, createRef } from "react";
+import { Component, createRef, lazy, Suspense } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Switch
 } from "react-router-dom";
 import API from "./utils/API";
-
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Logout from "./pages/Logout";
-import Dashboard from "./pages/Dashboard";
-import Disclaimer from "./pages/Disclaimer";
-import Server from "./pages/Server";
-import Audit from "./pages/Audit";
-
+import Loader from "./pages/Loader";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Status from "./components/Status";
-
 import "materialize-css/dist/css/materialize.min.css";
 import "./App.css";
 import "./Discord.css";
 import "./Kenny.css";
+
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Logout = lazy(() => import("./pages/Logout"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Disclaimer = lazy(() => import("./pages/Disclaimer"));
+const Server = lazy(() => import("./pages/Server"));
+const Audit = lazy(() => import("./pages/Audit"));
 
 class App extends Component {
   ref = createRef();
@@ -53,15 +52,17 @@ class App extends Component {
         <Router>
           <Status />
           {this.state.user ? <Navbar /> : undefined}
-          <Switch>
-            <Route path="/login" component={Login} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/dashboard" render={() => <Dashboard {...this.state} />} />
-            <Route path="/disclaimer" component={Disclaimer} />
-            <Route path="/server" render={() => <Server {...this.state} />} appRef={this.ref} />
-            <Route path="/audit" render={() => <Audit {...this.state} appRef={this.ref} />} />
-            <Route path="/" render={() => <Home {...this.state} />} />
-          </Switch>
+          <Suspense fallback={<Loader />}>
+            <Switch>
+              <Route path="/login" component={Login} />
+              <Route path="/logout" component={Logout} />
+              <Route path="/dashboard" render={() => <Dashboard {...this.state} />} />
+              <Route path="/disclaimer" component={Disclaimer} />
+              <Route path="/server" render={() => <Server {...this.state} />} appRef={this.ref} />
+              <Route path="/audit" render={() => <Audit {...this.state} appRef={this.ref} />} />
+              <Route path="/" render={() => <Home {...this.state} />} />
+            </Switch>
+          </Suspense>
           <Footer user={this.state.user} />
         </Router >
       </div>
