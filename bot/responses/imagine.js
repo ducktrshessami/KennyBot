f5ae9b6de0f6a5a919b7148569208a0ee74907dd
ducktrshessami <jwee761@gmail.com>
2021-06-11 19:48:03 -0700

    fixed imagine checking to be first few words

--- bot/responses/imagine.js
+++ bot/responses/imagine.js
@@ -12,7 +12,10 @@ catch {
     console.warn("Could not parse env BOT_DRAGONS as JSON");
 }
 
-module.exports = new Response(["imagine"], "Just imagine", undefined, cooldown(config.resOptions.cooldown, function (message, response) {
+module.exports = new Response("imagine", "Just imagine", function (message, trigger) {
+    let symbolIndex = message.cleanContent.match(/[$-/:-?{-~!"^_`\[\]]/) || {};
+    return message.cleanContent.slice(0, Math.min(16, symbolIndex.index || 16)).toLowerCase().trim().includes(trigger);
+}, cooldown(config.resOptions.cooldown, function (message, response) {
     utils.sendVerbose(message.channel, response)
         .catch(console.error);
 }), {
