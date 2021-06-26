0aae65af42f74001893119b2015f724b29ac7214
ducktrshessami <jwee761@gmail.com>
2021-06-26 16:18:31 -0700

    log setting audit

--- bot/commands/audit.js
+++ bot/commands/audit.js
@@ -1,9 +1,11 @@
 const { Command } = require("discord-bot");
 const db = require("../../models");
+const sendAudit = require("../utils/sendAudit");
 
 module.exports = new Command("audit", function (message) {
     db.Guild.findByPk(message.guild.id)
         .then(guild => guild.update({ auditChannel: message.channel.id }))
+        .then(() => sendAudit(message.guild.id, message.author, `designated <#${message.channel.id}> as the audit log channel`))
         .catch(console.error);
 }, {
     owner: true,
