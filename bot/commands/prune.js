265c6adfa3603e8f30e5cc8b7e595e6805d06c5d
ducktrshessami <jwee761@gmail.com>
2021-06-28 22:50:25 -0700

    prune perms

--- bot/commands/prune.js
+++ bot/commands/prune.js
@@ -38,6 +38,7 @@ module.exports = new Command("prune", function (message, args) {
             .catch(console.error);
     }
 }, {
+    requirePerms: "MANAGE_MESSAGES",
     usage: "@kennybot prune <number> [user]",
     description: "Deletes up to a given number of messages. The deleted messages can be filtered to a specified user",
     subtitle: "Discord only allows up to 100 messages to be deleted at a time"
